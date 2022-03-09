d636a60dabe9ee8bb11490181c59814fafc4d23d
Matthew Watson <capnkraken016@gmail.com>
2022-03-08 19:56:27 -0500

    Updating README
    -adding to github guidelines
    -added contributors to bottom of page

--- SpaceRaids_yeaah/Assets/_Scripts/Rotator.cs
+++ SpaceRaids_yeaah/Assets/_Scripts/Rotator.cs
@@ -6,7 +6,10 @@ using UnityEngine;
 
 public class Rotator : MonoBehaviour
 {
-    
+    [Tooltip("Rotation about the x, y, and z axis.")]
+    /// <summary>
+    /// Rotation about the x, y, and z axis.
+    /// </summary>
     public Vector3 rotationVelocity;
 
     // Update is called once per frame
