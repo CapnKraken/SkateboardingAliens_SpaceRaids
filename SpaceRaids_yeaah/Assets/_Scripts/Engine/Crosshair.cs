605540a268f573df32ea44ac0ca4c162866adb87
DonovanLott <93689796+DonovanLott@users.noreply.github.com>
2022-03-15 21:27:35 -0400

    Harvestinh
    Madde PlayerMaterial script

---
+++ SpaceRaids_yeaah/Assets/_Scripts/Engine/Crosshair.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Script by Donovan
+//Attached to HUD>Panel>Crosshair
+
+public class Crosshair : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+
+    
+
+    void Start()
+    {
+        
+    }
+
+    void harvest()
+    {
+
+    }
+    
+    void Update()
+    {
+
+    }
+}
