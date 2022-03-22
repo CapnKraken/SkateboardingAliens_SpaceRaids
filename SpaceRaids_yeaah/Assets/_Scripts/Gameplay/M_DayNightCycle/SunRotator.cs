47e2a44d00248656c2d51f8ca2bac55d9706ea08
Matthew Watson <capnkraken016@gmail.com>
2022-03-22 19:22:01 -0400

    Rudimentary day night

--- SpaceRaids_yeaah/Assets/_Scripts/Gameplay/M_DayNightCycle/SunRotator.cs
+++ SpaceRaids_yeaah/Assets/_Scripts/Gameplay/M_DayNightCycle/SunRotator.cs
@@ -6,11 +6,33 @@ using UnityEngine;
 
 public class SunRotator : ManagedObject
 {
-    public Vector3 rotationalVelocity;
+    public float rotationalVelocity;
+
+    //When these numbers are passed
+    public float dawnThreshold;
+    public float duskThreshold;
 
     public void Update()
     {
-        transform.Rotate(rotationalVelocity);
+        transform.Rotate(new Vector3(rotationalVelocity * Time.deltaTime, 0, 0));
+
+        float current = transform.rotation.eulerAngles.x;
+
+        //Rollover
+        if(current > 360)
+        {
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+        }
+
+        if(current > dawnThreshold)
+        {
+            Notify(Category.GENERAL, "Daybreak");
+        }
+
+        if(current > duskThreshold)
+        {
+            Notify(Category.GENERAL, "Nightfall");
+        }
     }
 
     #region Notifications
